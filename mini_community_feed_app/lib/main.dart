e7b826e071e22c07829b588f532278c843bf618e
Shirshendu Das <shirshendu079@gmail.com>
2025-11-05 15:11:12 +0530

    Generate necessary project files

---
+++ mini_community_feed_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(MiniCommunityApp());
+}
+
+class MiniCommunityApp extends StatelessWidget {
+  const MiniCommunityApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(),
+    );
+  }
+}
